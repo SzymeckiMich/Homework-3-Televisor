6b7d205de341b04b58d20e57d6c0c95bb3d23679
Michał Szymecki <szymsonik@gmail.com>
2020-01-24 23:54:33 +0100

    Create project

---
+++ src/Televisor.java
@@ -0,0 +1,13 @@
+public class Televisor {
+    boolean isTvOn = false;
+
+    void turnOn(){
+        isTvOn = true;
+    }
+    void turnOff(){
+        isTvOn = false;
+    }
+    void showStatus(){
+        System.out.println("TV is ON: " + isTvOn);
+    }
+}
